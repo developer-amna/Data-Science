f3396bfb2ba72769d5419464bafaaa252fa385b3
developer-amna <hafizaamna.webdev@gmail.com>
2026-05-21 20:34:12 +0500

    Indexing,Slicing,Strides

--- library 1_practice_code.py
+++ library 1_practice_code.py
@@ -20,7 +20,7 @@ start_time = time.time()
 np_result = np_array + 5
 end_time = time.time()
 print("Numpy Array Time:", end_time - start_time)
-
+# Creating Arrays in numpy
 arr = np.array([1,2,3])
 print (arr)
 
@@ -36,3 +36,24 @@ print(np.diag([1,2,3]))
 np.array([1,2,3],dtype = float)
 arr = np.array([1.5,2.7,3.9])
 print(arr.astype(int))
+
+# Numpy Indexing, Slicing & Strides
+
+arr = np.array([[10,20,30],
+                [40,50,60],
+                [70,80,90]])
+print(arr[0,1])
+print(arr[2,2])
+print(arr[-1,-2])
+print(arr[-3,-3])
+
+print(arr[0:2,1:3])
+print(arr[::2,::2])
+
+slice_arr = arr[0,:2]
+slice_arr[0] = 999
+print(arr[0])
+
+copy_arr = arr[1].copy()
+copy_arr[1] = 555
+print(arr[1])
